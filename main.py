c680af3996c60a1825b8a9cf93681ce4b8d51b0c
Noah Jaleeludeen <126204563+FriedSnakes@users.noreply.github.com>
2023-03-14 20:23:34 +0000

    Added function to calculate cost per serving

--- main.py
+++ main.py
@@ -7,23 +7,62 @@
   TIP: Use assessment guide to help guide you through this Internal
 '''
 
-# Create an empty list to store ingredient costs
-ingredient_costs = []
 
-# Get user input for ingredient names and costs
-def main():
+# Create an empty list to store ingredient costs
+def get_ingredient_costs():
+  ingredient_costs = []
+
+  # Get user input for ingredient names and costs
   num_ingredients = int(input("How many ingredients are in the recipe? "))
   for i in range(num_ingredients):
-      ingredient_name = input("Enter the name of ingredient #" + str(i+1) + ": ")
-      ingredient_cost = float(input("Enter the cost of " + ingredient_name + ": $"))
-      ingredient_quantity = float(input("Enter the quantity of " + ingredient_name + ": "))
-      ingredient_costs.append((ingredient_name, ingredient_cost, ingredient_quantity))
-  
-  # Calculate the total cost of ingredients
-  total_cost = 0
-  for ingredient in ingredient_costs:
+    ingredient_name = input("Enter the name of ingredient #" + str(i + 1) +
+                            ": ")
+    ingredient_cost = float(
+      input("Enter the cost of " + ingredient_name + ": $"))
+    ingredient_quantity = float(
+      input("Enter the quantity of " + ingredient_name + ": "))
+    ingredient_costs.append(
+      (ingredient_name, ingredient_cost, ingredient_quantity))
+
+  return ingredient_costs
+
+
+def main():
+  while True:
+    ingredient_costs = get_ingredient_costs()
+
+    # Calculate the total cost of ingredients
+    total_cost = 0
+    for ingredient in ingredient_costs:
       total_cost += ingredient[1] * ingredient[2]
+
+    # Print the total cost
+    print("The total cost of ingredients is: $" + str(total_cost))
+
+    # Ask user if they want to retry
+    retry = input("Do you want to retry? (Y/N) ")
+    if retry.lower() != "y":
+
+      # cost per serving calculator
+
+      # get user inputs
+      recipe_name = input("Enter the name of the recipe: ")
+      recipe_yield = float(input("Enter the yield (number of servings) of the recipe: "))
+      
+      
+      # calculate cost per serving
+      cost_per_serving = total_cost / recipe_yield
+      
+      # print results
+      print("\n")
+      print("Recipe name: ", recipe_name)
+      print("Recipe yield: ", recipe_yield, " servings")
+      print("Total cost: $", format(total_cost, ".2f"))
+      print("Cost per serving: $", format(cost_per_serving, ".2f"))
+      break
+
   
-  # Print the total cost
-  print("The total cost of ingredients is: $" + str(total_cost))
+main()
+
+input
 
