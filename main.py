a469bfd8c133a4438e674d09a18bb4272789e646
Noah Jaleeludeen <126204563+FriedSnakes@users.noreply.github.com>
2023-03-21 20:50:29 +0000

    Added alot. Ie allows user to choose what action they want to use (calculate total cost of ingredients OR cost per serving of a meal), and will keep prompting the user to choose a function until they choose to quit the program. Added a function to clear console

--- main.py
+++ main.py
@@ -8,70 +8,107 @@
 '''
 
 
+
+#function to record ingredient name and cost
+import json
+import os
+
+def file(ingredient_name, ingredient_cost):
+    ingredient_list = {ingredient_name: ingredient_cost}
+    file_name = input("What do you want the file name to be? ")
+    try:
+        with open(file_name, "w") as f:
+            f.write(json.dumps(ingredient_list))
+    except Exception as e:
+        print("An error occurred while writing to the file:", e)
+
+   
+       
 # Create an empty list to store ingredient costs
-def get_ingredient_costs():
-  ingredient_costs = []
-
-  # Get user input for ingredient names and costs
-  num_ingredients = int(input("How many ingredients are in the recipe? "))
-  for i in range(num_ingredients):
-    ingredient_name = input("Enter the name of ingredient #" + str(i + 1) +
-                            ": ")
-    ingredient_cost = float(
-      input("Enter the cost of " + ingredient_name + ": $"))
-    ingredient_quantity = float(
-      input("Enter the quantity of " + ingredient_name + ": "))
-    ingredient_costs.append(
-      (ingredient_name, ingredient_cost, ingredient_quantity))
-
-  return ingredient_costs
-
-
-def main():
+def ingredient_cost_calculator():
   while True:
-    ingredient_costs = get_ingredient_costs()
-
+    ingredient_costs = []
+    
+    # Get user input for ingredient names and costs
+    num_ingredients = int(input("\nHow many ingredients are in the recipe? "))
+    for i in range(num_ingredients):
+        ingredient_name = input("Enter the name of ingredient #" + str(i+1) + ": ")
+        ingredient_cost = float(input("Enter the cost of " + ingredient_name + ": $"))
+        ingredient_quantity = float(input("Enter the quantity of " + ingredient_name + ": "))
+        ingredient_costs.append((ingredient_name, ingredient_cost, ingredient_quantity))
+    
     # Calculate the total cost of ingredients
     total_cost = 0
     for ingredient in ingredient_costs:
-      total_cost += ingredient[1] * ingredient[2]
-
+        total_cost += ingredient[1] * ingredient[2]
+    
     # Print the total cost
     print("The total cost of ingredients is: $" + str(total_cost))
 
     # Ask user if they want to retry
     retry = input("Do you want to retry? (Y/N) ")
-    if retry.lower() != "y":
-
-      # cost per serving calculator
-
-      # get user inputs
-      recipe_name = input("Enter the name of the recipe: ")
-      recipe_yield = float(input("Enter the yield (number of servings) of the recipe: "))
-      
-      
-      # calculate cost per serving
-      cost_per_serving = total_cost / recipe_yield
-      
-      # print results
-      print("\n")
-      print("Recipe name: ", recipe_name)
-      print("Recipe yield: ", recipe_yield, " servings")
-      print("Total cost: $", format(total_cost, ".2f"))
-      print("Cost per serving: $", format(cost_per_serving, ".2f"))
+    if retry.lower() == "y":
+      continue
+    else:
+      print("")
+      print("")
+      print("")
       break
+
+def cost_per_serving_calculator():
+  while True:
+    # Step 1: Define variables
+    total_cost = 0.0
+    num_servings = 0
+    cost_per_serving = 0.0
+    
+    # Step 2: Get user input
+    total_cost = float(input("Enter the total cost of the meal: "))
+    num_servings = int(input("Enter the number of servings the meal can make: "))
+    
+    # Step 3: Calculate the cost per serving
+    cost_per_serving = total_cost / num_servings
+    
+    # Step 4: Print the result
+    print("The cost per serving of the meal is: $", format(cost_per_serving, '.2f'))
+    print("")
+    print("")
+    print("")
+    break
+
+#function to clear console
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+    
+#Title
+
+print("____________Meal Cost Calculator!____________")
+print("")
+print("")
+print("")
+
 
 while True:
-    print("Type 1 to calculate the cost of total ingredients, 2 to calculate the cost per serving of a meal, or 3 to quit the program")
-    user_action = input("What would you like to do?")
-    if user_action == '1':
-      print("dog")
-    elif user_action == '2':
-      print("cat")
-    elif user_action == '3':
-      break
-    else:
-      print("Invalid option")
-      continue
-   
-       
+  print("Welcome to the meal cost calculator. You have 4 options before you")
+  print("Option 1: Calculate the total cost of ingredients")
+  print("Option 2: Calculate the average cost per serving of a meal")
+  print("Option 3: Quit the program")
+  print("Option 4: Clear your console")
+  user_action = input("What would you like to do?\n")
+  if user_action == '1':
+    print("\nYou are calculating the total ingredient cost of a meal\n")
+    ingredient_cost_calculator()
+    continue
+  elif user_action == '2':
+    print("\nYou are calculating the cost per serving of a meal\n")
+    cost_per_serving_calculator()
+    continue
+  elif user_action == '3':
+    print("\nQuitting program!")
+    break
+  elif user_action == '4':
+    cls()
+    continue
+  else:
+    print("Invalid option\n")
+    continue
